874cfb04697b1dff7254938808fe34cdc851f0e9
Tos0301 <tos-141sousche@outlook.jp>
2025-05-30 22:47:39 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+
+from flask import Flask, render_template, request, redirect, url_for, session
+import random, time
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+IMAGES = [f'item{i}.jpg' for i in range(1, 25)]  # 24枚
+TRIALS = [{'image': img, 'position': 'left'} for img in IMAGES[:12]] + \
+         [{'image': img, 'position': 'right'} for img in IMAGES[12:]]
+random.shuffle(TRIALS)
+
+@app.route('/')
+def index():
+    session['trials'] = TRIALS.copy()
+    session['results'] = []
+    return redirect(url_for('trial'))
+
+@app.route('/trial', methods=['GET', 'POST'])
+def trial():
+    if request.method == 'POST':
+        data = {
+            'image': request.form['image'],
+            'position': request.form['position'],
+            'choice': request.form['choice'],
+            'timestamp': time.time()
+        }
+        session['results'].append(data)
+    if not session['trials']:
+        return redirect(url_for('complete'))
+    current = session['trials'].pop()
+    return render_template('trial.html', image=current['image'], position=current['position'])
+
+@app.route('/complete')
+def complete():
+    return render_template('complete.html', results=session['results'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
